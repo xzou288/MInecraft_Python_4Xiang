982efde07be5cecced8bd574d33bb38706c336a8
xzou288 <33523643+xzou288@users.noreply.github.com>
2017-12-07 16:57:11 +0000

    Add files via upload

---
+++ shower.py
@@ -0,0 +1,23 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+shwrX = 104.5
+shwrY = 1.0
+shwrZ = 109.5
+
+width = 5
+height = 5
+length = 5
+
+pos = mc.player.getTilePos()
+x = pos.x
+y = pos.y
+z = pos.z
+
+if shwrX <= x < shwrX + width and shwrY <= y < shwrY + height and shwrZ <= z < shwrZ + length
+    mc.setBlocks(shwrX, shwrY + height, shwrZ,
+                shwrX + width, shwrY + height, shwrZ + length, 8)
+    
+else:
+    mc.setBlocks(shwrX, shwrY + height, shwrZ,
+                shwrX + width, shwrY + height, shwrZ + length, 0)
